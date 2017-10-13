da2367003b25f1ccf515053b761d3f8d3343f2d6
Paulius R <paulius.repsys@visma.com>
2017-10-13 10:00:38 +0300

    Renamed namespace

--- public_api.ts
+++ public_api.ts
@@ -3,10 +3,10 @@
  * Build an Angular library compatible with AoT compilation & Tree shaking.
  * Written by Roberto Simonetti.
  * MIT license.
- * https://github.com/robisim74/angular-library-starter
+ * https://github.com/robisim74/tendsign-angular-components
  */
 
 /**
  * Entry point for all public APIs of the package.
  */
-export * from './src/angular-library-starter';
+export * from './src/tendsign-angular-components';
